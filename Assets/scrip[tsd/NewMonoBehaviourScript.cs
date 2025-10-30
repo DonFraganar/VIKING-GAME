7bc56d8120822aca7d586c508689ba5baed09901
Denis <denisfidelman1@gmail.com>
2025-10-30 19:31:07 +0200

    sdfg

--- Assets/scrip[tsd/NewMonoBehaviourScript.cs
+++ Assets/scrip[tsd/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //terst2
     }
 
     // Update is called once per frame
